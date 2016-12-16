924fb01b208718d665349e3f6d05b1eeada43e00
Rory Ashford <rory@roikles.com>
2016-12-16 15:39:07 +0000

    Init Commit!

---
+++ src/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section class="examples">
+    
+    <article class="margins">
+        <h1 class="fs2">Margin</h1>
+        <div class="m0 bg-navy c-white">m0</div>
+        <div class="m1 bg-navy c-white">m1</div>
+        <div class="m2 bg-navy c-white">m2</div>
+        <div class="m3 bg-navy c-white">m3</div>
+        <div class="m4 bg-navy c-white">m4</div>
+        <div class="m5 bg-navy c-white">m5</div>
+    </article>
+
+     <article class="margins">
+        <h1 class="fs2">Margin Top</h1>
+        <div class="mt0 bg-navy c-white">mt0</div>
+        <div class="mt1 bg-navy c-white">mt1</div>
+        <div class="mt2 bg-navy c-white">mt2</div>
+        <div class="mt3 bg-navy c-white">mt3</div>
+        <div class="mt4 bg-navy c-white">mt4</div>
+        <div class="mt5 bg-navy c-white">mt5</div>
+    </article>
+
+    <article class="margins">
+        <h1 class="fs2">Margin Right</h1>
+        <div class="mr0 bg-navy c-white">mr0</div>
+        <div class="mr1 bg-navy c-white">mr1</div>
+        <div class="mr2 bg-navy c-white">mr2</div>
+        <div class="mr3 bg-navy c-white">mr3</div>
+        <div class="mr4 bg-navy c-white">mr4</div>
+        <div class="mr5 bg-navy c-white">mr5</div>
+    </article>
+
+    <article class="margins">
+        <h1 class="fs2">Margin Bottom</h1>
+        <div class="mb0 bg-navy c-white">mr0</div>
+        <div class="mb1 bg-navy c-white">mr1</div>
+        <div class="mb2 bg-navy c-white">mr2</div>
+        <div class="mb3 bg-navy c-white">mr3</div>
+        <div class="mb4 bg-navy c-white">mr4</div>
+        <div class="mb5 bg-navy c-white">mr5</div>
+    </article>
+
+    <article class="margins">
+        <h1 class="fs2">Margin X (left and right</h1>
+        <div class="mx0 bg-navy c-white">mr0</div>
+        <div class="mx1 bg-navy c-white">mr1</div>
+        <div class="mx2 bg-navy c-white">mr2</div>
+        <div class="mx3 bg-navy c-white">mr3</div>
+        <div class="mx4 bg-navy c-white">mr4</div>
+        <div class="mx5 bg-navy c-white">mr5</div>
+    </article>
+
+    <article class="margins">
+        <h1 class="fs2">Margin Y (top and bottom)</h1>
+        <div class="my0 bg-navy c-white">mr0</div>
+        <div class="my1 bg-navy c-white">mr1</div>
+        <div class="my2 bg-navy c-white">mr2</div>
+        <div class="my3 bg-navy c-white">mr3</div>
+        <div class="my4 bg-navy c-white">mr4</div>
+        <div class="my5 bg-navy c-white">mr5</div>
+    </article>
+
+</section>
+
+
+
+
+</body>
