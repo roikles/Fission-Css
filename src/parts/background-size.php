fdc301c03698de7894dabdbc665cc252fa46c015
Rory Ashford <rory@roikles.com>
2017-01-04 12:20:22 +0000

    wrapped some html examples in grid

--- src/parts/background-size.php
+++ src/parts/background-size.php
@@ -1,10 +1,17 @@
 <h2 class="fs5 my1">Background Size</h2>
 
-<h3 class="fs6 fw-l my1">Auto (Default)</h3>
-<div class="bgr-nr bgs-a" style="background-image: url(https://source.unsplash.com/user/roikles/800x300); width:300px; height: 200px; background-color: #ccc""></div>
+<div class="g">
+    <div class="gi--1-1  gi--1-3--m">
 
-<h3 class="fs6 fw-l my1">Cover</h3>
-<div class="bgr-nr bgs-cv" style="background-image: url(https://source.unsplash.com/user/roikles/800x300); width:300px; height: 200px; background-color: #ccc""></div>
-
-<h3 class="fs6 fw-l my1">Contain</h3>
-<div class="bgr-nr bgs-c" style="background-image: url(https://source.unsplash.com/user/roikles/800x300); width:300px; height: 200px; background-color: #ccc""></div>
+        <h3 class="fs6 fw-l my1">Auto (Default)</h3>
+        <div class="bgr-nr bgs-a w100" style="background-image: url(https://source.unsplash.com/user/roikles/800x300); height: 200px; background-color: #ccc""></div>
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my1 w100">Cover</h3>
+        <div class="bgr-nr bgs-cv" style="background-image: url(https://source.unsplash.com/user/roikles/800x300); height: 200px; background-color: #ccc""></div>
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my1 w100">Contain</h3>
+        <div class="bgr-nr bgs-c" style="background-image: url(https://source.unsplash.com/user/roikles/800x300);height: 200px; background-color: #ccc""></div>
+    </div>
+</div>
--- src/parts/margin.php
+++ src/parts/margin.php
@@ -1,56 +1,67 @@
 
 <h2 class="fs5 my2">Margins</h2>
 
-<h3 class="fs6 fw-l my2">Margin</h3>
+<div class="g">
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my2">Margin</h3>
+        <div class="m0 p1 bg-blue c-white">.m0</div>
+        <div class="m1 p1 bg-blue c-white">.m1</div>
+        <div class="m2 p1 bg-blue c-white">.m2</div>
+        <div class="m3 p1 bg-blue c-white">.m3</div>
+        <div class="m4 p1 bg-blue c-white">.m4</div>
+        <div class="m5 p1 bg-blue c-white">.m5</div>
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my2">Margin Top</h3>
+        <div class="mt0 p1 bg-blue c-white">.mt0</div>
+        <div class="mt1 p1 bg-blue c-white">.mt1</div>
+        <div class="mt2 p1 bg-blue c-white">.mt2</div>
+        <div class="mt3 p1 bg-blue c-white">.mt3</div>
+        <div class="mt4 p1 bg-blue c-white">.mt4</div>
+        <div class="mt5 p1 bg-blue c-white">.mt5</div>
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my2">Margin Right</h3>
+        <div class="mr0 p1 bg-blue c-white">.mr0</div>
+        <div class="mr1 p1 bg-blue c-white">.mr1</div>
+        <div class="mr2 p1 bg-blue c-white">.mr2</div>
+        <div class="mr3 p1 bg-blue c-white">.mr3</div>
+        <div class="mr4 p1 bg-blue c-white">.mr4</div>
+        <div class="mr5 p1 bg-blue c-white">.mr5</div>
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my2">Margin Bottom</h3>
+        <div class="mb0 p1 bg-blue c-white">.mb0</div>
+        <div class="mb1 p1 bg-blue c-white">.mb1</div>
+        <div class="mb2 p1 bg-blue c-white">.mb2</div>
+        <div class="mb3 p1 bg-blue c-white">.mb3</div>
+        <div class="mb4 p1 bg-blue c-white">.mb4</div>
+        <div class="mb5 p1 bg-blue c-white">.mb5</div>
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my2">Margin X (left and right)</h3>
+        <div class="mx0 p1 bg-blue c-white">.mx0</div>
+        <div class="mx1 p1 bg-blue c-white">.mx1</div>
+        <div class="mx2 p1 bg-blue c-white">.mx2</div>
+        <div class="mx3 p1 bg-blue c-white">.mx3</div>
+        <div class="mx4 p1 bg-blue c-white">.mx4</div>
+        <div class="mx5 p1 bg-blue c-white">.mx5</div> 
+    </div>
+    <div class="gi--1-1  gi--1-3--m">
+        <h3 class="fs6 fw-l my2">Margin Y (top and bottom)</h3>
+        <div class="my0 p1 bg-blue c-white">.my0</div>
+        <div class="my1 p1 bg-blue c-white">.my1</div>
+        <div class="my2 p1 bg-blue c-white">.my2</div>
+        <div class="my3 p1 bg-blue c-white">.my3</div>
+        <div class="my4 p1 bg-blue c-white">.my4</div>
+        <div class="my5 p1 bg-blue c-white">.my5</div>
+    </div>
+</div>
 
-<div class="m0 p1 bg-blue c-white">.m0</div>
-<div class="m1 p1 bg-blue c-white">.m1</div>
-<div class="m2 p1 bg-blue c-white">.m2</div>
-<div class="m3 p1 bg-blue c-white">.m3</div>
-<div class="m4 p1 bg-blue c-white">.m4</div>
-<div class="m5 p1 bg-blue c-white">.m5</div>
 
-<h3 class="fs6 fw-l my2">Margin Top</h3>
 
-<div class="mt0 p1 bg-blue c-white">.mt0</div>
-<div class="mt1 p1 bg-blue c-white">.mt1</div>
-<div class="mt2 p1 bg-blue c-white">.mt2</div>
-<div class="mt3 p1 bg-blue c-white">.mt3</div>
-<div class="mt4 p1 bg-blue c-white">.mt4</div>
-<div class="mt5 p1 bg-blue c-white">.mt5</div>
 
-<h3 class="fs6 fw-l my2">Margin Right</h3>
 
-<div class="mr0 p1 bg-blue c-white">.mr0</div>
-<div class="mr1 p1 bg-blue c-white">.mr1</div>
-<div class="mr2 p1 bg-blue c-white">.mr2</div>
-<div class="mr3 p1 bg-blue c-white">.mr3</div>
-<div class="mr4 p1 bg-blue c-white">.mr4</div>
-<div class="mr5 p1 bg-blue c-white">.mr5</div>
 
-<h3 class="fs6 fw-l my2">Margin Bottom</h3>
 
-<div class="mb0 p1 bg-blue c-white">.mb0</div>
-<div class="mb1 p1 bg-blue c-white">.mb1</div>
-<div class="mb2 p1 bg-blue c-white">.mb2</div>
-<div class="mb3 p1 bg-blue c-white">.mb3</div>
-<div class="mb4 p1 bg-blue c-white">.mb4</div>
-<div class="mb5 p1 bg-blue c-white">.mb5</div>
 
-<h3 class="fs6 fw-l my2">Margin X (left and right)</h3>
-
-<div class="mx0 p1 bg-blue c-white">.mx0</div>
-<div class="mx1 p1 bg-blue c-white">.mx1</div>
-<div class="mx2 p1 bg-blue c-white">.mx2</div>
-<div class="mx3 p1 bg-blue c-white">.mx3</div>
-<div class="mx4 p1 bg-blue c-white">.mx4</div>
-<div class="mx5 p1 bg-blue c-white">.mx5</div>
-
-<h3 class="fs6 fw-l my2">Margin Y (top and bottom)</h3>
-
-<div class="my0 p1 bg-blue c-white">.my0</div>
-<div class="my1 p1 bg-blue c-white">.my1</div>
-<div class="my2 p1 bg-blue c-white">.my2</div>
-<div class="my3 p1 bg-blue c-white">.my3</div>
-<div class="my4 p1 bg-blue c-white">.my4</div>
-<div class="my5 p1 bg-blue c-white">.my5</div>
