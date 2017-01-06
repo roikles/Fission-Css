289283b41a80dcd780b21e2b67f23d06c25b8ea4
Rory Ashford <rory@roikles.com>
2017-01-06 12:18:30 +0000

    Added additional font styles to docs

--- src/parts/typography.php
+++ src/parts/typography.php
@@ -1,7 +1,6 @@
 <h1 id="typography" class="fs5 bd-b c-brand-blue">Typography</h1>
-<h3 class="fs4 fw-l">Typographic Scale</h3>
 
-<p>there is a range of type sizes to use:</p>
+<h2 class="fs4 fw-l">Typographic Scale</h2>
 
 <div class="fl fl-init fl-justify-between w100">
     <dl>
@@ -37,3 +36,77 @@
         <dd class="fs1">Size 1</dd>
     </dl>
 </div>
+
+<h3 class="fs4 fw-l">Responsive Typography (resize window to see)</h3>
+
+<h4>.fs3 / .fs4-m / .fs5-l</h4>
+<p class="fs3 fs4-m fs5-l">What's the point of going out? We're just going to wind up back here anyway. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Son, a woman is like a beer. They smell good, they look good, you'd step over your own mother just to get one! But you can't stop at one. You wanna drink another woman!</p>
+
+<h4>.fs2 / .fs3-m / .fs4-l</h4>
+<p class="fs2 fs3-m fs4-l">What's the point of going out? We're just going to wind up back here anyway. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Son, a woman is like a beer. They smell good, they look good, you'd step over your own mother just to get one! But you can't stop at one. You wanna drink another woman!</p>
+
+<h4>.fs1 / .fs2-m / .fs3-l</h4>
+<p class="fs1 fs2-m fs3-l">What's the point of going out? We're just going to wind up back here anyway. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Son, a woman is like a beer. They smell good, they look good, you'd step over your own mother just to get one! But you can't stop at one. You wanna drink another woman!</p>
+
+<h3 class="fs4 fw-l">Font Family</h3>
+
+<h4>Monospace</h4>
+<p class="ff-mono">What's the point of going out? We're just going to wind up back here anyway. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Son, a woman is like a beer.</p>
+
+<h4>Serif</h4>
+<p class="ff-serif">What's the point of going out? We're just going to wind up back here anyway. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Son, a woman is like a beer.</p>
+
+<h4>Sans Serif</h4>
+<p class="ff-sans">What's the point of going out? We're just going to wind up back here anyway. Lisa, vampires are make-believe, like elves, gremlins, and Eskimos. Son, a woman is like a beer.</p>
+
+<h3 class="fs4 fw-l">Text Decoration</h3>
+
+<p class="td-none">None</p>
+<p class="td-underline">Underline</p>
+<p class="td-overline">Overline</p>
+<p class="td-strike">Strike</p>
+
+.list{ list-style: none; }
+.list-bullet{ list-style: disc; }
+.list-inline{ display: flex; flex-direction: row; }
+
+<h3 class="fs4 fw-l">Lists</h3>
+
+<h4>List</h4>
+
+<ul class="list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+</ul>
+
+<h4>Bullet List</h4>
+
+<ul class="list-bullet">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+</ul>
+
+<h4>Inline List</h4>
+
+<ul class="list list-inline">
+    <li class="pr1">Item 1</li>
+    <li class="pr1">Item 2</li>
+    <li class="pr1">Item 3</li>
+    <li>Item 4</li>
+</ul>
+
+.tt-none{ text-transform: none; }
+    .tt-caps{ text-transform: capitalize; }
+    .tt-upper{ text-transform: uppercase; }
+    .tt-lower{ text-transform: lowercase; }
+
+<h3 class="fs4 fw-l">Text Transform</h3>
+
+<p class="tt-caps">capitalize</p>
+<p class="tt-upper">Uppercase</p>
+<p class="tt-lower">Lowercase</p>
+
